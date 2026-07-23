60c2785b4a8c34808d4b10fa6f72219a7ba457c3
seum8660 <seum8660@gmail.com>
2026-07-23 11:27:05 +0900

    update 2026-07-23 11:27:05.49

--- upload.bat
+++ upload.bat
@@ -3,6 +3,7 @@ chcp 65001 >nul
 cd /d "C:\Users\user\Documents\GitHub\school_bible"
 git add -A
 git commit -m "update %date% %time%"
+git pull --no-rebase -X ours --no-edit
 git push
 echo.
 echo ===== 업로드 완료 =====
